f34ccf393e710f468774316018e8e606aecd01d7
jun-ia <jundullah.ilhaq@gmail.com>
2022-10-08 14:09:15 +0700

    Initial Commit

---
+++ social-it/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  social-it
+//
+//  Created by Jundullah Ilhaq Aulia on 08/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
