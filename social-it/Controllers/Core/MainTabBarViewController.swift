55fa7ff3e291eef03c040a682b6ce7e95a90d87b
jun-ia <jundullah.ilhaq@gmail.com>
2022-10-09 08:07:24 +0700

    setup home page

--- social-it/Controllers/Core/MainTabBarViewController.swift
+++ social-it/Controllers/Core/MainTabBarViewController.swift
@@ -12,8 +12,25 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .systemBackground
         
-        view.backgroundColor = .systemGreen
+        let homeVC = UINavigationController(rootViewController: HomeViewController())
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        homeVC.tabBarItem.selectedImage = UIImage(systemName: "house.fill")
+
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        
+        let notificationVC = UINavigationController(rootViewController: NorificationViewController())
+        notificationVC.tabBarItem.image = UIImage(systemName: "bell")
+        notificationVC.tabBarItem.selectedImage = UIImage(systemName: "bell.fill")
+        
+        let directMessageVC = UINavigationController(rootViewController: DirectMessageViewController())
+        directMessageVC.tabBarItem.image = UIImage(systemName: "envelope")
+        directMessageVC.tabBarItem.selectedImage = UIImage(systemName: "envelope.fill")
+        
+        setViewControllers([homeVC, searchVC, notificationVC, directMessageVC], animated: true)
+        
     }
 }
 
