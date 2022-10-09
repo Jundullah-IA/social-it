3aaf778d70a17dc8be54106286ccd80be454a5f6
jun-ia <jundullah.ilhaq@gmail.com>
2022-10-09 08:05:22 +0700

    Setup project

---
+++ social-it/Controllers/Core/NotificationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NorificationViewController.swift
+//  social-it
+//
+//  Created by Jundullah Ilhaq Aulia on 08/10/22.
+//
+
+import UIKit
+
+class NorificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
